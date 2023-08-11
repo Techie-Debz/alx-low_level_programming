acbc88ae0cabbd1f53bec15e27520d598f0df52e
Techie-Debz <triciachiwen@yahoo.com>
2023-08-11 08:22:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print out output with put
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
