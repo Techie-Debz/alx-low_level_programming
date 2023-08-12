250bf847782488c1ea7081882f1e63365fae3b87
Techie-Debz <triciachiwen@yahoo.com>
2023-08-11 22:00:01 -0700

    hhhh

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - Prints the alphabet without q and e.
- *
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
  * Return: Always 0 (Success)
-/*
+ */
 int main(void)
 {
-        int i;
-	
-	for (i = 0; i < 10; i++)
-	{
-		print("%d", 1);
-	}
-	putchar('\n');
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
 	return (0);
 }
-	 
+
