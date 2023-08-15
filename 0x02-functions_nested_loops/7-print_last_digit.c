63feeb7d6e4ec680a4fd510aaf7715846cc77fe0
Techie-Debz <triciachiwen@yahoo.com>
2023-08-15 14:50:32 -0700

    7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,11 @@
  */
 int print_last_digit(int i)
 {
-        int k;
-        k = i % 10;
-        if (i < 0)
-                k = -k;
-        _putchar(k + '0');
-        return (k);
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
 }
