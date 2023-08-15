f9acf9670153d17858d7860b93e64871e03d208a
Techie-Debz <triciachiwen@yahoo.com>
2023-08-15 14:12:12 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,26 @@
 #include "main.h"
 
 /**
- *  _isalpha - Checks for alphabetic character
- *  @c: The character to be checked
- *  Return: 1 for alphabetic character or -for anything else
+ *  print_sign - prints the sign of a number
+ *  @n: The character to be checked
+ *  Return: 1
  */
 
 int print_sign(int n)
-
 {
-
 	if (n > 0)
 	{
-	_putchar(43);
-	return (1);
+		_putchar('+');
+		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-	_putchar(45);
-	return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-	_putchar(48);
-	return (0);
+		_putchar('-');
+		return (-1);
 	}
 }
